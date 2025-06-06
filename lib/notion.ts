118dcaa888e83fe1c0abe7c9028e538e34257a85
Gonçalo Dias <goncalodias.studio@gmail.com>
2025-06-06 08:08:50 +0100

    ✨ Implement functions to fetch content from Notion

--- lib/notion.ts
+++ lib/notion.ts
@@ -1,4 +1,40 @@
 import { Client } from '@notionhq/client';
+import { PageObjectResponse } from '@notionhq/client/build/src/api-endpoints';
+
 export const notion = new Client({
   auth: process.env.NOTION_API_KEY,
 });
+
+function isPageObjectResponse(response: any): response is PageObjectResponse {
+  return !!response.properties;
+}
+
+export async function fetchDatabaseContent(id: string) {
+  const data = await notion.databases.query({ database_id: id });
+
+  return data.results.filter(isPageObjectResponse);
+}
+
+export async function fetchPageContent(id: string) {
+  const data = await notion.pages.retrieve({ page_id: id });
+
+  return data;
+}
+
+export async function fetchBlockContent(id: string) {
+  const { results } = await notion.blocks.children.list({
+    block_id: id,
+  });
+
+  const blocks: any[] = await Promise.all(
+    results.map(async (block: any) => {
+      if (block.has_children) {
+        const children = await fetchBlockContent(block.id);
+        return { ...block, children };
+      }
+      return block;
+    }),
+  );
+
+  return blocks;
+}
