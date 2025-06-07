d4c3311bf2929a03e4c36ab40436a0ec05dbf4d8
Gonçalo Dias <goncalodias.studio@gmail.com>
2025-06-07 14:38:58 +0100

    ✨ Display formatted article date

--- app/articles/[slug]/page.tsx
+++ app/articles/[slug]/page.tsx
@@ -5,6 +5,7 @@ import {
   fetchDatabaseContent,
   fetchPageContent,
 } from '@/lib/notion';
+import { formatDate } from '@/lib/utils';
 
 export default async function Article({
   params,
@@ -22,13 +23,20 @@ export default async function Article({
 
   return (
     <Container className="max-w-(--breakpoint-md) gap-20 py-20">
-      <header className="flex flex-col gap-5">
+      <header className="flex flex-col gap-3">
         <h1 className="font-medium">
           {(page.properties.Name as any).title[0].plain_text}
         </h1>
         <p className="text-foreground-neutral-subtle">
           {(page.properties.Summary as any).rich_text[0].plain_text}
         </p>
+        <span className="text-foreground-neutral-faded text-body-small font-mono font-medium uppercase">
+          {formatDate((page.properties.Date as any).date.start, {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+          })}
+        </span>
       </header>
       <article className="flex flex-col gap-2">
         {blocks.map((block: any) => (
