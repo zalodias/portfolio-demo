b34c1f201c0b0ae5976f5a55b52d70d9e292c3dc
Gonçalo Dias <goncalodias.studio@gmail.com>
2025-06-08 19:30:22 +0100

    📝 Update profile description

--- data/profile.ts
+++ data/profile.ts
@@ -8,5 +8,5 @@ export const profile: Profile = {
   name: 'Gonçalo Dias',
   title: 'Software Designer',
   description:
-    'Blending design intuition with technical precision to build polished digital experiences. With a background in both design & development, I care deeply about usability, performance, and clarity — and I‘m always chasing that sweet spot where form meets function.',
+    'Blending design intuition with technical precision to build polished digital experiences. I care deeply about usability, performance, and clarity — and I‘m always chasing that sweet spot where form meets function.',
 };
